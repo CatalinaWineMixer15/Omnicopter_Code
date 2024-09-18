7baf404e80461ccf49cb46326ac26d8b83164f66
David Evert <david.evert19@gmail.com>
2024-09-18 13:59:18 -0400

    Fixed errors with mavserver and StartUpD documentation

--- scripts/mavserver.py
+++ scripts/mavserver.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+
 
 from multiprocessing import Process
 import subprocess
 
-# Forms IP:port strings
-def ipportstr(IP, port):
-    return IP + ":" + str(port)
+
+# Forms IP:port or path:baud strings
+def pathstr(part1, part2):
+    return part1 + ":" + str(part2)
 
 
 # Runs mavlink-router to form the connections
@@ -15,22 +18,23 @@ def runmavrouter():
     GCS_port2 = 14560
     GCS_port3 = 14570
 
-    PX4_IP = "/dev/ttyUSB0"
-    PX4_port = 921600
+    FC_path = "/dev/ttyUSB0"
+    FC_baud = 921600
 
-    GCS_str1 = ipportstr(GCS_IP, GCS_port1)
-    GCS_str2 = ipportstr(GCS_IP, GCS_port2)
-    GCS_str3 = ipportstr(GCS_IP, GCS_port3)
-    PX4_str = ipportstr(PX4_IP, PX4_port)
+    GCS_str1 = pathstr(GCS_IP, GCS_port1)
+    GCS_str2 = pathstr(GCS_IP, GCS_port2)
+    GCS_str3 = pathstr(GCS_IP, GCS_port3)
+    FC_str = pathstr(FC_path, FC_baud)
 
     print("GCS_str1: ", GCS_str1)
     print("GCS_str2: ", GCS_str2)
     print("GCS_str3: ", GCS_str3)
-    print("PX4_str: ", PX4_str)
+    print("FC_str: ", FC_str)
 
-    subprocess.run(["mavlink-routerd", "-e", GCS_str1, "-e", GCS_str2, "-e", GCS_str3, PX4_str])
+    subprocess.run(["mavlink-routerd", "-e", GCS_str1, "-e", GCS_str2, "-e", GCS_str3, FC_str])
 
 
+# Main program
 if __name__ == '__main__':
 
     process1 = Process(target=runmavrouter)
